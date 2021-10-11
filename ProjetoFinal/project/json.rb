86c8b3e4e5b92805b1d94c1f519cbe16d131602f
Hiarpa Neto <hiarpanetto@gmail.com>
2021-10-11 19:00:00 -0400

    fix: req http

---
+++ ProjetoFinal/project/json.rb
@@ -0,0 +1,7 @@
+headers1 = {key: '9edbc30109a14f6087eaab70be3accc9', region: 'brazilsouth', content_type: 'application/json'}
+
+headers = {"Ocp-Apim-Subscription-Key" => '9edbc30109a14f6087eaab70be3accc9', "Ocp-Apim-Subscription-Region" => 'brazilsouth', "Content-Type" =>'application/json'}
+
+puts headers1.class
+
+puts headers.inspect
--- ProjetoFinal/project/test_req.rb
+++ ProjetoFinal/project/test_req.rb
@@ -3,24 +3,20 @@ require 'rest-client'
 require 'json'
 
 #Constantes do Azure Translator
-subscription_key = "9edbc30109a14f6087eaab70be3accc9"
-endpoint = "https://api.cognitive.microsofttranslator.com"
-location = "brazilsouth"
-content_type = "application/json"
-path = "/translate?api-version=3.0&to=it"
+subscription_key = '9edbc30109a14f6087eaab70be3accc9'
+endpoint = 'https://api.cognitive.microsofttranslator.com'
+region = 'brazilsouth'
+content_type = 'application/json'
+path = '/translate?api-version=3.0&to=it'
 constructed_url = endpoint + path
 
-headers = {
-    'Ocp-Apim-Subscription-Key': subscription_key,
-    'Ocp-Apim-Subscription-Region': location,
-    'Content-Type': 'application/json'
-}
+headers = {"Ocp-Apim-Subscription-Key" => '9edbc30109a14f6087eaab70be3accc9', "Ocp-Apim-Subscription-Region" => 'brazilsouth', "Content-Type" =>'application/json'}
 
-body = [{
-    "Text": "Hello World!"
-}]
+text = '[{"Text": "Bom dia Hiarpa, como está ?"}]'
 
-#Primeiro envio da frase a ser traduzida e a linguagem.
-puts constructed_url
-res = RestClient.post "#{constructed_url}", headers, body
+puts text
+puts headers
 
+res = RestClient.post "#{constructed_url}",text, headers
+puts res.code
+puts res.body
